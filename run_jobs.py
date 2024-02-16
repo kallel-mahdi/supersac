9f22f958b7ecfc2ad4a3d3b429dc8d4eb0032d57
Mahdi <mahdi.kallel@uni-wuerzburg.de>
2024-02-16 17:05:16 +0100

    bad one

--- run_jobs.py
+++ run_jobs.py
@@ -2,7 +2,17 @@ import subprocess
 import itertools
 import argparse
 import numpy as np
-from jaxrl_m.utils import str2bool
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+    
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed',type=int,default=42) 
--- run_supersac.py
+++ run_supersac.py
@@ -8,7 +8,17 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 
-from jaxrl_m.utils import str2bool
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+    
 
 def none_or_str(value):
     if value == 'None':

a217127e71ee5965fa5a5df19193045f059fc658
kallel-mahdi <mahdi.kallel@uni-wuerzburg.de>
2024-04-22 15:29:59 +0200

    trying to decorrelate critics to make valdiation criterion work better

--- run_supersac_envpool.py
+++ run_supersac_envpool.py
@@ -13,32 +13,12 @@ import envpool
 logging.basicConfig(level=logging.CRITICAL)
 
 
-
-def get_batch(i,batches_vmap):
-
-    #return  jax.tree_map(lambda x: x[i], batches)
-    f = lambda batches : jax.tree_map(lambda x: x[i], batches)
-    tmp = lambda batches_vmap: jax.vmap(f)(batches_vmap)
-
-    return tmp(batches_vmap)
-
+def get_batch(i,batches):
+    return  jax.tree_map(lambda x: x[i], batches)
 
 def body(i,val):
     agent,batches = val
-    tmp = get_batch(i,batches)
     return (agent.update_critics(get_batch(i,batches)),batches)
-
-def generate_batches(rng,transitions):
-
-    rngs = jax.random.split(rng, 5)
-    tmp = lambda rng : jax.random.choice(rng,a=transitions['observations'].shape[0], shape=(5000,256), replace=True)
-    tmp2 = lambda  idxs : jax.vmap(lambda i: jax.tree_map(lambda x: x[i], transitions))(idxs)
-    idxs_vmap = jax.vmap(tmp)(rngs)
-    batches = jax.vmap(tmp2)(idxs_vmap)
-
-    return batches
-
-    
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -65,21 +45,20 @@ os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 parser = argparse.ArgumentParser()
 parser.add_argument('--algo_name', type=str, default='sac', help='the name of the RL algorithm')
 parser.add_argument('--seed',type=int,default=42) 
-parser.add_argument('--env_name',type=str,default="Hopper-v5") 
+parser.add_argument('--env_name',type=str,default="Walker2d-v4") 
 parser.add_argument('--project_name',type=str,default="delete") 
-parser.add_argument('--gamma',type=float,default=0.99)
-parser.add_argument('--max_steps',type=int,default=1_000_000) 
-parser.add_argument('--num_rollouts',type=int,default=5) 
+parser.add_argument('--gamma',type=float,default=0.995)
+parser.add_argument('--max_steps',type=int,default=5_000_000) 
+parser.add_argument('--num_rollouts',type=int,default=50) 
 parser.add_argument('--num_critics',type=int,default=5)     
-parser.add_argument('--adaptive_critics',type=str2bool,default=True) 
+parser.add_argument('--adaptive_critics',type=str2bool,default=False) 
 parser.add_argument('--discount_entropy',type=str2bool,default=True) 
 parser.add_argument('--discount_actor',type=str2bool,default=True) 
-parser.add_argument('--max_episode_steps',type=int,default=500) 
-parser.add_argument('--entropy_coeff',type=float,default=1.) 
+parser.add_argument('--max_episode_steps',type=int,default=1000) 
+parser.add_argument('--entropy_coeff',type=float,default=0.5) 
 parser.add_argument('--actor_lr',type=float,default=3e-4) 
 parser.add_argument('--temp_lr',type=float,default=3e-4) 
 parser.add_argument('--healthy_reward',type=float,default=1.)
-parser.add_argument('--use_momentum',type=bool,default=False)
 
 args = parser.parse_args()
 
@@ -134,8 +113,7 @@ class SACAgent(flax.struct.PyTreeNode):
         
         new_rng, curr_key, next_key = jax.random.split(agent.rng, 3)
 
-        #def update_one_critic(critic):
-        def update_one_critic(critic,batch):
+        def update_one_critic(critic):
                             
                 def critic_loss_fn(critic_params):
                         
@@ -163,21 +141,30 @@ class SACAgent(flax.struct.PyTreeNode):
                 return new_critic,critic_info
 
 
-        #new_critics,critic_info = jax.vmap(update_one_critic)(agent.critic)
-        new_critics,critic_info = jax.vmap(update_one_critic,in_axes=(0,0))(agent.critic,batch)
-
-
+        new_critics,critic_info = jax.vmap(update_one_critic)(agent.critic)
         agent = agent.replace(rng=new_rng,critic=new_critics)
         
         return agent
     
     #@jax.jit
     partial(jax.jit, static_argnums=(3,))
-    def update_critics_seq(agent,batches):
+    def update_critics_seq(agent,batches,R2,reset):
         
+        ### Reset  the weights of worst performing critic
+        mask = jnp.zeros(( agent.config["num_critics"],))
+        
+        if agent.config['adaptive_critics'] and reset: 
+            
+                mask = mask.at[jnp.argmin(R2)].set(1)
+            
+        rngs = jax.random.split(agent.rng, agent.config["num_critics"])    
+        reset = lambda rng,params : agent.critic.init(rng,agent.config["observations"], agent.config["actions"])["params"]
+        no_reset = lambda rng,params : params
+        f= lambda mask,rng,params : lax.cond(mask,reset,no_reset,rng,params)
+        new_critic_params = jax.vmap(f,in_axes=(0,0,0))(mask,rngs,agent.critic.params)
         ### Reset optimizers 
-        new_opt_state = jax.vmap(agent.critic.tx.init)(agent.critic.params)
-        new_critics = agent.critic.replace(params=agent.critic.params,opt_state=new_opt_state)
+        new_opt_state = jax.vmap(agent.critic.tx.init)(new_critic_params)
+        new_critics = agent.critic.replace(params=new_critic_params,opt_state=new_opt_state)
         agent = agent.replace(critic=new_critics)
         ### Train critic sequentially
         agent,batches = jax.lax.fori_loop(0,5000,body,(agent,batches))
@@ -189,9 +176,13 @@ class SACAgent(flax.struct.PyTreeNode):
         new_rng, curr_key, next_key = jax.random.split(agent.rng, 3)
 
         def actor_loss_fn(actor_params,R2):
-            observations = jnp.repeat(batch['observations'], 10, axis=0)
-            discounts = jnp.repeat(batch['discounts'], 10, axis=0)
-            masks = jnp.int32(jnp.repeat(batch['masks'], 10, axis=0))
+            # observations = jnp.repeat(batch['observations'], 10, axis=0)
+            # discounts = jnp.repeat(batch['discounts'], 10, axis=0)
+            # masks = jnp.int32(jnp.repeat(batch['masks'], 10, axis=0))
+
+            observations = batch['observations']
+            discounts = batch['discounts']
+            masks = batch['masks']
 
             dist = agent.actor(observations, params=actor_params)
             actions, log_probs = dist.sample_and_log_prob(seed=curr_key)
@@ -248,6 +239,7 @@ class SACAgent(flax.struct.PyTreeNode):
                        observations: np.ndarray,
                        seed: PRNGKey,
                        temperature: float = 1.0,
+                       random: bool = False,
                        ) -> jnp.ndarray:
         
         ### random always true
@@ -268,13 +260,12 @@ def create_learner(
                 discount_entropy,
                 adaptive_critics,
                 entropy_coeff,
-                use_momentum,
+                
                 actor_lr: float = 3e-4,
                 critic_lr: float = 3e-4,
                 temp_lr: float =1e-3,## Test
                 hidden_dims: Sequence[int] = (256, 256),
                 target_entropy: float = None,
-                
             **kwargs):
 
         print('Extra kwargs:', kwargs)
@@ -292,20 +283,16 @@ def create_learner(
         critics = jax.vmap(TrainState.create,in_axes=(None,0,None))(critic_def,critic_params,optax.adam(learning_rate=critic_lr))
 
         actor_params = actor_def.init(actor_key, observations)['params']
-
+        
+        #actor = TrainState.create(actor_def, actor_params, tx=optax.adam(learning_rate=actor_lr))
+        actor = TrainState.create(actor_def, actor_params, tx=optax.rmsprop(learning_rate=actor_lr))
+        
+        
         temp_def = Temperature()
         temp_params = temp_def.init(rng)['params']
+        #temp = TrainState.create(temp_def, temp_params, tx=optax.adam(learning_rate=temp_lr))
+        temp = TrainState.create(temp_def, temp_params, tx=optax.rmsprop(learning_rate=temp_lr))
         
-        if use_momentum:
-
-            actor = TrainState.create(actor_def, actor_params, tx=optax.adam(learning_rate=actor_lr))
-            temp = TrainState.create(temp_def, temp_params, tx=optax.adam(learning_rate=temp_lr))
-
-        else:
-
-            actor = TrainState.create(actor_def, actor_params, tx=optax.rmsprop(learning_rate=actor_lr))
-            temp = TrainState.create(temp_def, temp_params, tx=optax.rmsprop(learning_rate=temp_lr))
-            
         
         if target_entropy is None:
             target_entropy = -entropy_coeff*action_dim
@@ -318,6 +305,7 @@ def create_learner(
             discount_actor = discount_actor, 
             discount_entropy = discount_entropy,
             adaptive_critics = adaptive_critics,
+            #critic_def = critic_def,    
         ))
 
         return SACAgent(rng, critic=critics, target_critic=critics, actor=actor, temp=temp, config=config)
@@ -361,12 +349,19 @@ def train(args):
     
     
     ### HalfCheetah does not have healthy_reward argument
-    if 'HalfCheetah' in args.env_name or 'Swimmer' in args.env_name:
-        env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=args.max_episode_steps))
-    else:
-        print(f'env_name: {args.env_name}, max_episode_steps: {args.max_episode_steps}, healthy_reward: {args.healthy_reward}')
-        env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=args.max_episode_steps,healthy_reward=args.healthy_reward))
-    eval_env = EpisodeMonitor(gym.make(args.env_name))
+    # if 'HalfCheetah' in args.env_name or 'Swimmer' in args.env_name:
+    #     env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=args.max_episode_steps))
+    # else:
+    #     print(f'env_name: {args.env_name}, max_episode_steps: {args.max_episode_steps}, healthy_reward: {args.healthy_reward}')
+    #     env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=args.max_episode_steps,healthy_reward=args.healthy_reward))
+    # eval_env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=1000))
+    
+    # env = EpisodeMonitor(gym.make(args.env_name,max_episode_steps=args.max_episode_steps))
+    # eval_env = EpisodeMonitor(gym.make(args.env_name))
+    
+    env = envpool.make(args.env_name, env_type="gymnasium", num_envs=args.num_rollouts)
+    eval_env = envpool.make(args.env_name, env_type="gymnasium", num_envs=10)
+    
 
     example_transition = dict(
         observations=env.observation_space.sample(),
@@ -393,7 +388,6 @@ def train(args):
                     entropy_coeff=args.entropy_coeff,
                     temp_lr=args.temp_lr,
                     actor_lr=args.actor_lr,
-                    use_momentum=args.use_momentum,
                     hidden_dims=hidden_dims,
                     #**FLAGS.config
                     )
@@ -403,9 +397,10 @@ def train(args):
     exploration_rng = jax.random.PRNGKey(0)
     i = 0
     unlogged_steps,cached_steps = 0,0
-    policy_rollouts = deque([], maxlen=20)
+    policy_rollouts = deque([], maxlen=25)
     warmup = True
     R2,bias = jnp.ones(args.num_critics),jnp.zeros(args.num_critics)
+
     
     with tqdm.tqdm(total=max_steps) as pbar:
         
@@ -414,7 +409,7 @@ def train(args):
                 warmup=(i < start_steps)
                 
                 logging.debug('policy rollout')
-                replay_buffer,actor_buffer,policy_rollout,policy_return,variance,undisc_policy_return,num_steps = rollout_policy(
+                replay_buffer,actor_buffer,policy_rollout,policy_return,variance,undisc_policy_return,num_steps = rollout_policy_parallel(
                                                                         agent,env,exploration_rng,
                                                                         replay_buffer,actor_buffer,warmup=warmup,
                                                                         num_rollouts=args.num_rollouts,discount = args.gamma,max_length=args.max_episode_steps)
@@ -430,14 +425,14 @@ def train(args):
                     ### Update critics ###:
                     logging.debug('update critics')
                     transitions = replay_buffer.get_all()
-                    # idxs = jax.random.choice(agent.rng,a=transitions['observations'].shape[0], shape=(5000,256), replace=True)
-                    # batches = jax.vmap(lambda i: jax.tree_map(lambda x: x[i], transitions))(idxs)
-                    batches = generate_batches(agent.rng,transitions)
-                    agent = agent.update_critics_seq(batches)
+                    idxs = jax.random.choice(agent.rng,a=transitions['observations'].shape[0], shape=(5000,256), replace=True)
+                    batches = jax.vmap(lambda i: jax.tree_map(lambda x: x[i], transitions))(idxs)
+                    reset = jnp.min(R2) < 0.
+                    agent = agent.update_critics_seq(batches,R2,reset)
                         
                     ### Update critic weights ## 
                     logging.debug('update critic weights')
-                    if len(policy_rollouts)>=20 and agent.config["adaptive_critics"]:   
+                    if len(policy_rollouts)>=25 and agent.config["adaptive_critics"]:   
                     
                         flattened_rollouts = flatten_rollouts(policy_rollouts)
                         R2,bias = evaluate_many_critics(agent,policy_rollout.policy_return,flattened_rollouts)
@@ -483,9 +478,15 @@ def train(args):
                     
                     if unlogged_steps >= log_interval:
                         
-                        policy_fn = partial(supply_rng(agent.sample_actions), temperature=0.)
-                        eval_metrics = evaluate(policy_fn, eval_env, num_episodes=10)
-                        eval_metrics = {f'evaluation/{k}': v for k, v in eval_metrics.items()}
+                        _,_,policy_rollout,policy_return,variance,undisc_policy_return,num_steps = rollout_policy_parallel(
+                                                                        agent,eval_env,exploration_rng,
+                                                                        None,None,warmup=False,
+                                                                        num_rollouts=10,random=True,
+                                                                        discount = args.gamma,max_length=1000)
+                        
+                        
+                        eval_metrics = {"policy_return": policy_return,"std": jnp.sqrt(variance),"undisc_policy_return": undisc_policy_return}
+                    
                         wandb.log(eval_metrics, step=int(i),commit=True)
                         unlogged_steps = 0
                 

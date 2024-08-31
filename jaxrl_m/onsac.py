4cf0233d1acdaef55a2c3248fbf5a2eceb17e823
kallel-mahdi <mahdi.kallel@uni-wuerzburg.de>
2024-08-31 11:12:27 +0200

    Runs : Czech2

--- jaxrl_m/onsac.py
+++ jaxrl_m/onsac.py
@@ -20,7 +20,7 @@ def body(i,val):
 
 class Temperature(nn.Module):
     #initial_temperature: float = -4.605 ## (log(0.01))
-    initial_temperature: float = 0.01
+    initial_temperature: float = 5e-3
     
     
     @nn.compact
@@ -191,7 +191,7 @@ class SACAgent(flax.struct.PyTreeNode):
             new_actor, actor_info = agent.actor.apply_loss_fn(actor_loss_fn,True,adv)
             new_temp, temp_info = agent.temp.apply_loss_fn(temp_loss_fn,True,actor_info['entropy'], agent.config['target_entropy'])
             agent = agent.replace(rng=new_rng, actor=new_actor,temp=new_temp)
-            new_temp.params["log_temp"]=jnp.clip(new_temp.params["log_temp"],0.01,1)
+            new_temp.params["log_temp"]=jnp.clip(new_temp.params["log_temp"],5e-3,1)
             #agent = agent.replace(temp=new_temp)
             
         

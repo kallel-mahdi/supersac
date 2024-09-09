d24579a39c64230ccdd6e375d0012b4ad9879936
kallel-mahdi <mahdi.kallel@uni-wuerzburg.de>
2024-09-09 17:23:55 +0200

    RUNS : ICLR_CLOSE

--- jaxrl_m/rollout.py
+++ jaxrl_m/rollout.py
@@ -47,7 +47,6 @@ def rollout_policy(agent,env,exploration_rng,
     observations,disc_masks,rewards = np.zeros((max_steps,obs.shape[0])),np.zeros((max_steps,)),np.zeros((max_steps,))
     policy_returns = np.zeros((num_rollouts,))
     
-    
     while n_rollouts < num_rollouts:
         
         if eval:
@@ -61,8 +60,6 @@ def rollout_policy(agent,env,exploration_rng,
         
         next_obs, reward, done, truncated, info = env.step(action)
         
-        #obs,action,done = obs.squeeze(),action.squeeze(),done.squeeze()
-        
         mask = float(not done)
 
         transition = dict(observations=obs,actions=action,
@@ -74,10 +71,7 @@ def rollout_policy(agent,env,exploration_rng,
         
         if actor_buffer is not None:
             actor_buffer.add_transition(transition)
-
-        
-
-        #print(max_length,n_rollouts,episode_step)
+    
         observations[max_length*n_rollouts+episode_step] = obs
         disc_masks[max_length*n_rollouts+episode_step] = disc
         rewards[max_length*n_rollouts+episode_step] = reward
@@ -87,7 +81,7 @@ def rollout_policy(agent,env,exploration_rng,
         episode_step += 1
         n_steps += 1
         
-        if (done or truncated or n_steps>=max_length) :
+        if (done or truncated) :
             policy_returns[n_rollouts] = (disc_masks[max_length*n_rollouts:max_length*(n_rollouts+1)]*rewards[max_length*n_rollouts:max_length*(n_rollouts+1)]).sum()
             obs,_= env.reset()
             n_rollouts += 1
@@ -98,17 +92,12 @@ def rollout_policy(agent,env,exploration_rng,
     policy_return = policy_returns.mean()
     variance = policy_returns.var()
     undisc_policy_return = rewards.sum()/num_rollouts
-    
-    if not eval : 
-        policy_rollout = PolicyRollout( policy_params=agent.actor.params,
+    policy_rollout = PolicyRollout( policy_params=agent.actor.params,
                                     policy_return=policy_return,
                                     variance=variance,
                                     observations=observations,
                                     disc_masks=disc_masks,
                                     num_rollouts=jnp.array(num_rollouts))
-        
-    
-    else : policy_rollout = None
     
     return replay_buffer,actor_buffer,policy_rollout,policy_return,variance,undisc_policy_return,n_steps
 
